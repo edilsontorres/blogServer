1e287ec2443313d8ab50351745d1d341645e9806
edilson torres <edilsontorres.job@gmail.com>
2024-02-23 20:03:26 -0300

    Criando CRUD para Postagem

--- Controllers/PostagensController.cs
+++ Controllers/PostagensController.cs
@@ -22,12 +22,56 @@ namespace blog.Controllers
             return Ok(posts);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> ListPostById([FromRoute]int id)
+        {
+            var idPost = await _context.Posts.FindAsync(id);
+            if(idPost != null)
+            {
+                return Ok(idPost);
+            }
+
+            return BadRequest("Sem reultados para pesquisa");
+        }
+
         [HttpPost("novapostagem")]
         public async Task<ActionResult<Post>> NewPost([FromBody] Post posts)
         {
             _context.Add(posts);
             await _context.SaveChangesAsync();
             return Ok(201);
+        }
+
+        [HttpPut("editarpostagem/{id}")]
+        public async Task<ActionResult> ToEdit([FromRoute]int id, [FromBody] Post posts)
+        {
+            var idPost = await _context.Posts.FindAsync(id);
+            if(idPost != null)
+            {
+                idPost.Title = posts.Title;
+                idPost.Content = posts.Content;
+                idPost.Author = posts.Author;
+
+                _context.Posts.Update(idPost);
+                await _context.SaveChangesAsync();
+                return Ok("Dados atualizado com sucesso!");
+            }
+
+            return BadRequest("Erro ao indexar postagem");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemovePost([FromRoute]int id)
+        {
+            var idPost = await _context.Posts.FindAsync(id);
+            if(idPost != null)
+            {
+                _context.Posts.Remove(idPost);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest("Sem reultados para pesquisa");
         }
 
         
