5a7e8f48e47fec0ca3aa7dfa978e8e8f7c059221
edilson <edilsontor77@gmail.com>
2024-07-31 22:18:44 -0300

    Atualizando DB

---
+++ Migrations/20240801004037_TabelaDataCriacao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaDataCriacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImg",
+                table: "Posts",
+                type: "VARCHAR(255)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(255)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CriatedAt",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 31, 21, 40, 37, 270, DateTimeKind.Local).AddTicks(8557));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastDateUpdate",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 31, 21, 40, 37, 270, DateTimeKind.Local).AddTicks(9054));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CriatedAt",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "LastDateUpdate",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImg",
+                table: "Posts",
+                type: "VARCHAR(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(255)");
+        }
+    }
+}
---
+++ Migrations/20240801011250_AtualizandoNomeTabelaData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoNomeTabelaData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CriatedAt",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastDateUpdate",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValue: new DateTime(2024, 7, 31, 21, 40, 37, 270, DateTimeKind.Local).AddTicks(9054));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastDateUpdate",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 31, 21, 40, 37, 270, DateTimeKind.Local).AddTicks(9054),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CriatedAt",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 31, 21, 40, 37, 270, DateTimeKind.Local).AddTicks(8557));
+        }
+    }
+}
